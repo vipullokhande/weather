1f0c2d354f42179df3093fd687514327a41e2213
Vipul Balu Lokhande <99359962+vipullokhande@users.noreply.github.com>
2023-12-04 17:55:48 +0530

    Update secrets.dart

--- lib/secrets/secrets.dart
+++ lib/secrets/secrets.dart
@@ -1,4 +1,4 @@
 var ak =
-    'https:/api.openweathermap.org/data/2.5/forecast?id=1275339&appid=07fa88dbf4794d7273b2b60406b58e06';
+    'https:/api.openweathermap.org/data/2.5/forecast?id=1275339&appid={API_KEY}';
 String apiUrl(String city) =>
     'https://api.openweathermap.org/data/2.5/weather?q=$city&appid={API_KEY}';
