4cfecd24fbb48d5c1faacfdd1d86904f69039ba1
Vipul <vipullokhande1@gmail.com>
2023-12-04 17:53:21 +0530

    Second Commit

--- lib/resources/weather_api.dart
+++ lib/resources/weather_api.dart
@@ -1,16 +1,12 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:weather/manage_cities_page.dart';
+import 'package:weather/secrets/secrets.dart';
 
 class WeatherApi {
   Future<Model> getData(String city) async {
-    // var ak =
-    //     'https:/api.openweathermap.org/data/2.5/forecast?id=1275339&appid=07fa88dbf4794d7273b2b60406b58e06';
-    var apiUrl =
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=07fa88dbf4794d7273b2b60406b58e06';
     try {
-      final response = await http.get(Uri.parse(apiUrl));
+      final response = await http.get(Uri.parse(apiUrl(city)));
       var data = await jsonDecode(response.body);
       return Model(
         cityName: data['name'],
